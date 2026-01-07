fa0a85671d29f4aa52031c4a594226326813f9e6
esobirjon2197 <esobirjon2197@gmail.com>
2026-01-07 16:13:09 +0500

    Add list examples with index and slicing operations

---
+++ main.py
@@ -0,0 +1,30 @@
+# 1-m
+i = ["kitob", "daftar", "qalam", "o'chirg'ich"]
+print(i)
+print(i.index("qalam"))
+
+
+# 2-m
+i = [5,10,15,20,25]
+print(i)
+print(i.index(15))
+
+
+# 3-m
+i = ["a", "b", "c", "d", "e"]
+print(i)
+print(i.index("d"))
+
+
+# 4-m
+i = [100,200,300,400]
+print(i)
+print(i.index(1))
+
+
+# 5-m
+i = ["olma", "banan", "nok"]
+print(i)
+print(i[::-2])
+
+
